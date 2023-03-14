30430ca25fe25eeb6f2464300ec9da9d62bb496c
hehoku <hehoku@outlook.com>
2023-03-14 17:46:00 +0800

    feat: content script get bookmark data url

--- src/backgroundPage.ts
+++ src/backgroundPage.ts
@@ -8,3 +8,20 @@ browser.runtime.onMessage.addListener((request: { popupMounted: boolean }) => {
         console.log("backgroundPage notified that Popup.tsx has mounted.");
     }
 });
+
+chrome.webRequest.onCompleted.addListener(
+    function (details) {
+        if (details.method == "GET" && details.url.includes("Bookmarks")) {
+            console.log(details.url);
+            chrome.tabs.query(
+                { active: true, currentWindow: true },
+                function (tabs: any) {
+                    return chrome.tabs.sendMessage(tabs[0].id, {
+                        bookmarkUrl: details.url,
+                    });
+                },
+            );
+        }
+    },
+    { urls: ["<all_urls>"] },
+);
--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,3 +1,11 @@
 window.onload = () => {
     console.log("hello");
 };
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.bookmarkUrl) {
+        console.log({ bookmarkUrl: request.bookmarkUrl });
+        sendResponse({ fromContentScript: "content script got url" });
+    }
+    console.log(request);
+});
